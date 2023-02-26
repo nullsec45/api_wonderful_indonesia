257fb58407320aa3b71ca3018e336dcbfac2472e
ramarff <rama21010ti@student.nurulfikri.ac.id>
2023-02-27 00:04:57 +0700

    ubah kota migrate

--- migration/kota.migrate.go
+++ migration/kota.migrate.go
@@ -1,1 +1,20 @@
-package main
+package migration
+
+import (
+	"api/database"
+	"api/models"
+)
+
+func KotaMigrate() {
+	db := database.Init()
+	var kota models.Kota
+
+	kotaTable := db.Migrator().HasTable(kota)
+
+	if !kotaTable {
+		db.AutoMigrate(kota)
+	}
+	sqlDB, _ := db.DB()
+	sqlDB.Close()
+
+}
