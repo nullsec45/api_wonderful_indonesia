92095b01750d335663a58043e6ee078fa37f8dab
ramarff <rama21010ti@student.nurulfikri.ac.id>
2023-02-27 00:05:19 +0700

    ubah file utamaa migration

--- migration/migration.go
+++ migration/migration.go
@@ -1,35 +1,10 @@
-package main
+package migration
 
-import (
-	"api_wonderful_indonesia/database"
-	"api_wonderful_indonesia/models"
-)
-
-func Migrate() bool {
-	db := database.Init()
-
-	var user models.User
-	var kabupaten models.Kabupaten
-	var kota models.Kota
-	var pakaianAdat models.PakaianAdat
-	var provinsi models.Provinsi
-	var role models.Role
-
-	//db.AutoMigrate(&models.User{}, &models.Kabupaten{}, &models.Kota{}, &models.PakaianAdat{}, &models.Provinsi{})
-	userTable := db.Migrator().HasTable(user)
-	kabupatenTable := db.Migrator().HasTable(kabupaten)
-	kotaTable := db.Migrator().HasTable(kota)
-	pakaianAdatTable := db.Migrator().HasTable(pakaianAdat)
-	provinsiTable := db.Migrator().HasTable(provinsi)
-	roleTable := db.Migrator().HasTable(role)
-
-	if !userTable && !kabupatenTable && !kotaTable && !pakaianAdatTable && !provinsiTable && !roleTable {
-		db.AutoMigrate(user, kabupaten, kota, pakaianAdat, provinsi, roleTable)
-		return true
-	} else {
-		//fmt.Println("Table sudah ada!")
-		//db.close()
-		return false
-	}
-
+func Migrate() {
+	KabupatenMigrate()
+	KotaMigrate()
+	UserMigrate()
+	PakaianAdatMigrate()
+	ProvinsiMigrate()
+	RoleMigrate()
 }
