77794041d150b7c9a422defdf5d981eb3c156394
ramarff <rama21010ti@student.nurulfikri.ac.id>
2023-02-27 00:05:48 +0700

    ubah file main menjadi api

--- api.go
+++ api.go
@@ -1,24 +1,36 @@
 package main
 
 import (
-	"api_wonderful_indonesia/database"
-	"api_wonderful_indonesia/routes"
+	"api/database"
+	"api/migration"
+	"api/routes"
 	"fmt"
+
 	"github.com/urfave/cli/v2"
 	"log"
 	"os"
 )
 
 func main() {
-	database.Init()
-	e := routes.Init()
-	e.Logger.Fatal(e.Start(":8080"))
-
 	app := &cli.App{
-		Name:  "greet",
-		Usage: "fight the loneliness!",
-		Action: func(*cli.Context) error {
-			fmt.Println("Hello friend!")
+		Name:  "api",
+		Usage: "Perintah ini digunakan untuk manajemen api",
+		Action: func(cCtx *cli.Context) error {
+			args := cCtx.Args().Get(0)
+			//fmt.Printf("Hello %q", cCtx.Args().Get(0))
+
+			switch args {
+			case "migrate":
+				migration.Migrate()
+			case "run":
+				database.Init()
+				e := routes.Init()
+				e.Logger.Fatal(e.Start(":8080"))
+			default:
+				fmt.Println("Selamat Datang di API Wonderful OF Indonesia")
+				fmt.Println("API ini berisikan tentang indahnya keberagaman yang ada di Indonesia")
+			}
+
 			return nil
 		},
 	}
