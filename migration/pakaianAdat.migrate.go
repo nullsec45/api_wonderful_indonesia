42a8751b96f36a0465e79ec199bc8405ad8f8213
ramarff <rama21010ti@student.nurulfikri.ac.id>
2023-02-27 00:04:05 +0700

    ubah migration pakaian adat

--- migration/pakaianAdat.migrate.go
+++ migration/pakaianAdat.migrate.go
@@ -1,1 +1,20 @@
-package main
+package migration
+
+import (
+	"api/database"
+	"api/models"
+)
+
+func PakaianAdatMigrate() {
+	db := database.Init()
+	var pakaianAdat models.PakaianAdat
+
+	pakaianAdatTable := db.Migrator().HasTable(pakaianAdat)
+
+	if !pakaianAdatTable {
+		db.AutoMigrate(pakaianAdat)
+	}
+	sqlDB, _ := db.DB()
+	sqlDB.Close()
+
+}
