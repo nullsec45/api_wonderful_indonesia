7806d27750e4567d28886d9c0c50d2f8947b4969
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-03-26 06:09:37 +0700

    menambahkan pointer password

--- helpers/password.helper.go
+++ helpers/password.helper.go
@@ -2,8 +2,8 @@ package helpers
 
 import "golang.org/x/crypto/bcrypt"
 
-func HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+func HashPassword(password *string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(*password), bcrypt.DefaultCost)
 	return string(bytes), err
 }
 
