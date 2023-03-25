681a4af3a5ffdcb9b39e1635aafd748fecd39fa7
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-03-26 06:10:04 +0700

    ubah init ke connectDatabase dan tambah pesan

--- migration/kabupaten.migrate.go
+++ migration/kabupaten.migrate.go
@@ -3,15 +3,17 @@ package migration
 import (
 	"api/database"
 	"api/models"
+	"fmt"
 )
 
 func KabupatenMigrate() {
-	db := database.Init()
+	db := database.ConnectDatabase()
 	var kabupaten models.Kabupaten
 	kabupatenTable := db.Migrator().HasTable(kabupaten)
 
 	if !kabupatenTable {
 		db.AutoMigrate(kabupaten)
+		fmt.Println("Berhasil membuat table daftar_kabupaten")
 	}
 
 	sqlDB, _ := db.DB()
