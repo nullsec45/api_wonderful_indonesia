5b0e238427bea7d7b7732be4f6dd07f757b7bde9
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-03-26 06:12:24 +0700

    ubah init ke connectDatabase dan tambah pesan

--- migration/user.migrate.go
+++ migration/user.migrate.go
@@ -3,14 +3,16 @@ package migration
 import (
 	"api/database"
 	"api/models"
+	"fmt"
 )
 
 func UserMigrate() {
-	db := database.Init()
+	db := database.ConnectDatabase()
 
 	var user models.User
 	userTable := db.Migrator().HasTable(user)
 	if !userTable {
 		db.AutoMigrate(user)
+		fmt.Println("Berhasil membuat table daftar_user")
 	}
 }
