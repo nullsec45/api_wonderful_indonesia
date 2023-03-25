c54c5403a61e4fba66200e5621ab0fef93225e99
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-03-26 06:15:14 +0700

    Perbaiki Model Role

--- models/Role.go
+++ models/Role.go
@@ -1,7 +1,10 @@
 package models
 
 type Role struct {
-	Id   int    `gorm:"type:integer;primaryKey"`
-	Role string `gorm:"size:150"`
-	Kode string `gorm:"type:char;size:2"`
+	Id   int    `gorm:"type:integer;primaryKey" json:"id"`
+	Role string `gorm:"size:150" json:"role"`
+}
+
+func (Role) TableName() string {
+	return "daftar_role"
 }
