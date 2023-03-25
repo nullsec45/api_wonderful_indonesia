832835a9fdb115a43604c6232f554f217403590e
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-03-26 06:13:54 +0700

    Pebaiki Pakaian Adat Model

--- models/PakaianAdat.go
+++ models/PakaianAdat.go
@@ -1,9 +1,13 @@
 package models
 
 type PakaianAdat struct {
-	Id            int    `gorm:"type:integer;primaryKey"`
-	NamaPakaian   string `gorm:"size:150"`
-	GambarPakaian string `gorm:"size:100"`
-	IdDaerah      int    `gorm:"type:integer;size:3"`
-	IdRole        int    `gorm:"type:integer;size:1;default:1"`
+	Id            int      `gorm:"type:integer;primaryKey" json:"id"`
+	NamaPakaian   string   `gorm:"size:100;column:nama_pakaian" json:"nama_pakaian" validate:"required""`
+	GambarPakaian string   `gorm:"size:255;column:gambar_pakaian" json:"nama_gambar" validate:"required"`
+	ProvinsiId    int      `gorm:"type:integer;column:id_provinsi"  json:"id_provinsi" validate:"required"`
+	Provinsi      Provinsi `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+}
+
+func (PakaianAdat) TableName() string {
+	return "daftar_pakaian_adat"
 }
