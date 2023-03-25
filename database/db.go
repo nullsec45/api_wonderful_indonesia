0c77501c1d6b276cc2d41aa1a998eafab36f2f03
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-03-26 06:08:54 +0700

    Perbaiki kode di db.go

--- database/db.go
+++ database/db.go
@@ -4,22 +4,18 @@ import (
 	"api/config"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"log"
 )
 
-var db *gorm.DB
-var err error
+var DB *gorm.DB
 
-func Init() *gorm.DB {
+func ConnectDatabase() *gorm.DB {
 	conf := config.GetConfig()
 
-	dsn := conf.DB_USERNAME + ":" + conf.DB_PASSWORD + "@tcp(" + conf.DB_HOST + ":" + conf.DB_PORT + ")/" + conf.DB_NAME + "?charset=utf8mb4&parseTime&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
+	dsn := conf.DB_USERNAME + ":" + conf.DB_PASSWORD + "@tcp(" + conf.DB_HOST + ":" + conf.DB_PORT + ")/" + conf.DB_NAME
+	db, err := gorm.Open(mysql.Open(dsn))
 	if err != nil {
-		log.Println("Connection failed", err)
-	} else {
-		log.Println("Connection established")
+		panic(err)
 	}
-	return db
+	DB = db
+	return DB
 }
