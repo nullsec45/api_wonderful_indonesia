0f64fe7b4694d9e2fbade784c21cc780f3c15bf3
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-03-26 06:13:26 +0700

    Perbaiki Kota Model

--- models/Kota.go
+++ models/Kota.go
@@ -1,8 +1,12 @@
 package models
 
 type Kota struct {
-	Id         int    `gorm:"type:integer;primaryKey"`
-	NamaKota   string `gorm:"size:150"`
-	IdProvinsi int    `gorm:"type:integer;size:2"`
-	IdRole     int    `gorm:"type:integer;size:1;default:5"`
+	Id         int      `gorm:"type:integer;primaryKey" json:"id"`
+	NamaKota   string   `gorm:"size:150" json:"nama_kota"`
+	ProvinsiId int      `gorm:"type:integer;column:id_provinsi"  json:"id_provinsi" validate:"required"`
+	Provinsi   Provinsi `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+}
+
+func (Kota) TableName() string {
+	return "daftar_kota"
 }
