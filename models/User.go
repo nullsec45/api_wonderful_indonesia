c02da39f27659a75476f7d526b55f28826d9de3d
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-03-26 06:16:07 +0700

    Perbaiki User Model

--- models/User.go
+++ models/User.go
@@ -1,8 +1,12 @@
 package models
 
 type User struct {
-	Id       int    `gorm:"type:integer;primaryKey"`
-	Username string `gorm:"size:255"`
-	Password string `gorm:"size:255"`
-	IdRole   int    `gorm:"size:2"`
+	Id       int    `gorm:"type:integer;primaryKey" json:"id"`
+	Email    string `gorm:"size:255" json:"email"`
+	Username string `gorm:"size:255" json:"username" validate:"required"`
+	Password string `gorm:"size:255" json:"password" validate:"required"`
+}
+
+func (User) TableName() string {
+	return "daftar_user"
 }
