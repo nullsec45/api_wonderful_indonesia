40ee4964afc101d65f49de314f45c6467e9f9287
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-03-26 06:13:14 +0700

    Perbaiki Kabupaten model

--- models/Kabupaten.go
+++ models/Kabupaten.go
@@ -1,8 +1,12 @@
 package models
 
 type Kabupaten struct {
-	Id            int    `gorm:"type:integer;primaryKey"`
-	NamaKabupaten string `gorm:"size:150"`
-	IdProvinsi    int    `gorm:"type:integer;size:2"`
-	IdRole        int    `gorm:"type:integer;size:1;default:5"`
+	Id            int      `gorm:"type:integer;primaryKey" json:"id"`
+	NamaKabupaten string   `gorm:"size:150;column:nama_kabupaten" json:"nama_kabupaten" validate:"required"`
+	ProvinsiId    int      `gorm:"type:integer;column:id_provinsi"  json:"id_provinsi" validate:"required"`
+	Provinsi      Provinsi `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+}
+
+func (Kabupaten) TableName() string {
+	return "daftar_kabupaten"
 }
